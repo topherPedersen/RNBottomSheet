0906152859820e0d2163da66a8f42814d23e34f3
Chris Pedersen <christopher.pedersen1@bswhealth.org>
2022-02-01 13:14:06 -0600

    break off JSX for BottomSheet content into new component <ScheduleNow />

--- App.js
+++ App.js
@@ -3,6 +3,35 @@ import { View, Text, StyleSheet, Button, SafeAreaView, TouchableWithoutFeedback 
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+const ScheduleNow = () => {
+  return(
+    <View style={styles.contentContainer}>
+      {/* BottomSheet Heading Container */}
+      <View style={{width: '100%', height: 50, alignContent: 'center', flexDirection: 'row', backgroundColor: 'white'}}>
+        <View style={{backgroundColor: 'white', width: '20%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+        </View>
+        <View style={{backgroundColor: 'white', width: '60%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{}}>Schedule Now</Text>
+        </View>
+        <View style={{backgroundColor: 'white', width: '20%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{color: 'blue'}}>X</Text>
+        </View>
+      </View>
+      {/* Horizontal Line (Divide BottomSheet Heading from BottomSheet Content) */}
+      <View style={{width: '100%', height: 1, backgroundColor: '#C0C0C0'}}></View>
+      {/* BottomSheet Cards */}
+      <View style={{width: '90%', height: 85, marginTop: 30, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
+      <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+        <TouchableWithoutFeedback onPress={ () => alert("TODO: Add the animation!") }>
+          <Text>Schedule a Service</Text>
+        </TouchableWithoutFeedback>
+      </View>
+      <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
+      <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
+    </View>
+  );
+};
+
 const App = () => {
   // ref
   const bottomSheetRef = useRef(null);
@@ -29,30 +58,7 @@ const App = () => {
         onChange={handleSheetChanges}
         enablePanDownToClose={true}
       >
-        <View style={styles.contentContainer}>
-          {/* BottomSheet Heading Container */}
-          <View style={{width: '100%', height: 50, alignContent: 'center', flexDirection: 'row', backgroundColor: 'white'}}>
-            <View style={{backgroundColor: 'white', width: '20%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-            </View>
-            <View style={{backgroundColor: 'white', width: '60%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <Text style={{}}>Schedule Now</Text>
-            </View>
-            <View style={{backgroundColor: 'white', width: '20%', height: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-              <Text style={{color: 'blue'}}>X</Text>
-            </View>
-          </View>
-          {/* Horizontal Line (Divide BottomSheet Heading from BottomSheet Content) */}
-          <View style={{width: '100%', height: 1, backgroundColor: '#C0C0C0'}}></View>
-          {/* BottomSheet Cards */}
-          <View style={{width: '90%', height: 85, marginTop: 30, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
-          <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableWithoutFeedback onPress={ () => alert("TODO: Add the animation!") }>
-              <Text>Schedule a Service</Text>
-            </TouchableWithoutFeedback>
-          </View>
-          <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
-          <View style={{width: '90%', height: 85, marginTop: 10, backgroundColor: 'white', borderColor: '#C0C0C0', borderWidth: 1, borderRadius: 10}}></View>
-        </View>
+        <ScheduleNow />
       </BottomSheet>
     </View>
   );
